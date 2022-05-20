05dd2ec6740d58ec83e4b6847cbb961bf6d631a5
angie0503 <80481395+angie0503@users.noreply.github.com>
2022-05-20 10:52:05 +0200

    Add files via upload

---
+++ Blatt 4_T/Transportmitteltest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "Transportmittel.h"
+using namespace std;
+
+int main()
+{
+	//Test Basisklasse
+	Transportmittel TM_1(1.8, 2.4);
+	TM_1.bewegen("Osten");
+
+	cout << "Das ist die Breite des Transportmittels: " << TM_1.get_breite() << endl;
+
+
+
+
+
+
+
+
+
+
+
+}
