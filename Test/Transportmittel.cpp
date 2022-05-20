be87324bfad108d8b6a29835ccf78ef427d53b36
angie0503 <80481395+angie0503@users.noreply.github.com>
2022-05-20 10:33:56 +0200

    Add files via upload

---
+++ Test/Transportmittel.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "Transportmittel.h"
+using namespace std;
+
+// allgeminer Konstruktor Transportmittel
+
+//Methode
+void Transportmittel ::bewegen(string richtung)
+{
+	string x = richtung;
+	cin >> x;
+
+		if (x == "Norden")
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+		else if (x == "Sueden")
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+		else if (x == "Osten")
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+		else if (x == "Westen")
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+}
+
+
+
+
+
+	
+
